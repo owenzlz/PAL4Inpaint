e8cadf0105e78ea74a154c91c9ed7d092d06c3f5
owenzlz <32859275+owenzlz@users.noreply.github.com>
2024-01-24 22:46:31 -0500

    Update download_checkpoints.sh

--- download_checkpoints.sh
+++ download_checkpoints.sh
@@ -1,4 +1,4 @@
 pip install gdown
-gdown https://drive.google.com/uc?id=1O1m5WpJhW6_l4Dm3v70hW1297zoAnpRQ
-mkdir ckpt
+gdown https://drive.google.com/file/d/1I5FX4wBPxELjxeJ_H34ugK-ALZq_SPj1/view?usp=sharing
+mkdir ./ckpt
 mv best_mIoU_iter_1800_torchscript.pth ./ckpt
