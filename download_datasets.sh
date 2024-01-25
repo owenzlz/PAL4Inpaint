c549872314b1f1fc91d5d439e6865fed5ed04149
owenzlz <32859275+owenzlz@users.noreply.github.com>
2024-01-24 22:44:52 -0500

    Update download_datasets.sh

--- download_datasets.sh
+++ download_datasets.sh
@@ -1,6 +1,6 @@
 pip install gdown
 mkdir datasets
 cd datasets
-gdown https://drive.google.com/uc?id=1QNvUzQajYs-_VxPRtq-Ze3RBP5xsuUJO
+gdown https://drive.google.com/file/d/1f--5ju-vuhxtzXlAvH3VJ7tyHk9rw4Bb/view?usp=sharing
 unzip PA4Inpaint_subset.zip
 rm PA4Inpaint_subset.zip
